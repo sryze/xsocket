bbac30712fcb30201ee6e5b76cd5c5d86af8e91e
szx <szx@null.net>
2020-11-15 20:59:26 +0600

    Improve http_get example

--- examples/http_get.c
+++ examples/http_get.c
@@ -7,6 +7,7 @@
 
 #define HTTP_REQUEST_SIZE 1024
 #define HTTP_RESPONSE_SIZE 100 * 1024
+#define HTTP_DEFAULT_PORT "80"
 
 #ifdef _WIN32
 
@@ -40,15 +41,20 @@ char *error_str(int error_code)
 
 #endif
 
-static int http_get(const char *hostname, const char *uri, char **response)
+static int http_get(const char *hostname,
+                    const char *port,
+                    const char *uri,
+                    char **request,
+                    char **response)
 {
   int error = 0;
   socket_t sock;
   struct addrinfo ai_hints, *ai_result = NULL, *ai_cur;
   char *addr_str = NULL;
-  char request_buf[HTTP_REQUEST_SIZE];
-  char *buf;
+  char *request_buf = NULL;
+  char *response_buf = NULL;
   size_t len;
+  char port_spec[7] = "";
 
   sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (sock == INVALID_SOCKET) {
@@ -61,7 +67,7 @@ static int http_get(const char *hostname, const char *uri, char **response)
   ai_hints.ai_socktype = SOCK_STREAM;
   ai_hints.ai_protocol = IPPROTO_TCP;
 
-  error = getaddrinfo(hostname, "80", &ai_hints, &ai_result);
+  error = getaddrinfo(hostname, port, &ai_hints, &ai_result);
   if (error != 0) {
     goto error_return;
   }
@@ -85,10 +91,25 @@ static int http_get(const char *hostname, const char *uri, char **response)
   freeaddrinfo(ai_result);
   ai_result = NULL;
 
+  if (strcmp(port, HTTP_DEFAULT_PORT) != 0) {
+    snprintf(port_spec, sizeof(port_spec), ":%s", port);
+  }
+
+  request_buf = calloc(1, HTTP_REQUEST_SIZE);
+  if (request_buf == NULL) {
+    error = errno;
+    goto error_return;
+  }
+
   snprintf(request_buf,
-           sizeof(request_buf),
-           "GET %s HTTP/1.0\r\n\r\n",
-           uri);
+           HTTP_REQUEST_SIZE,
+           "GET %s HTTP/1.0\r\n"
+           "Host: %s%s\r\n"
+           "Accept: */*\r\n"
+           "\r\n",
+           uri,
+           hostname,
+           port_spec);
   len = strlen(request_buf);
 
   error = (int)send_n(sock, request_buf, (int)len, 0);
@@ -97,19 +118,20 @@ static int http_get(const char *hostname, const char *uri, char **response)
     goto error_return;
   }
 
-  buf = calloc(1, HTTP_RESPONSE_SIZE);
-  if (buf == NULL) {
+  response_buf = calloc(1, HTTP_RESPONSE_SIZE);
+  if (response_buf == NULL) {
     error = errno;
     goto error_return;
   }
 
-  error = (int)recv_n(sock, buf, HTTP_RESPONSE_SIZE, 0, NULL);
+  error = (int)recv_n(sock, response_buf, HTTP_RESPONSE_SIZE, 0, NULL);
   if (error <= 0) {
     error = socket_error;
     goto error_return;
   }
 
-  *response = buf;
+  *request = request_buf;
+  *response = response_buf;
 
   close_socket_nicely(sock);
   free(addr_str);
@@ -117,6 +139,8 @@ static int http_get(const char *hostname, const char *uri, char **response)
   return 0;
 
 error_return:
+  free(response_buf);
+  free(request_buf);
   freeaddrinfo(ai_result);
   free(addr_str);
   close_socket_nicely(sock);
@@ -124,10 +148,28 @@ error_return:
   return error;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+  char *hostname;
+  char *port;
+  char *path;
+  char *request;
   char *response;
   int error;
+
+  if (argc <= 1) {
+    fprintf(stderr,
+      "Usage: %s <hostname> <port> <path>\n\n"
+      "Examples:\n\n"
+      "%s google.com 80 /\n"
+      "%s youtube.com 80 /feed/trending\n\n",
+      argv[0], argv[0], argv[0]);
+    return 1;
+  }
+
+  hostname = argv[1];
+  port = argv[2];
+  path = argv[3];
 
   error = socket_init();
   if (error != 0) {
@@ -135,13 +177,16 @@ int main()
     return 1;
   }
 
-  error = http_get("google.com", "/", &response);
+  error = http_get(hostname, port, path, &request, &response);
   if (error != 0) {
     fprintf(stderr, "Error: %s\n", error_str(error));
     return 1;
   }
 
-  printf("%s\n", response);
+  puts(request);
+  free(request);
+
+  puts(response);
   free(response);
 
   socket_cleanup();
